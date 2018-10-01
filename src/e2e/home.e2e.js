65684c5400a8095f069f8b6480eea8ab7b014d29
Rayron Victor <rayronvictor@gmail.com>
2018-10-01 13:46:40 -0300

    fix tests

--- src/e2e/home.e2e.js
+++ src/e2e/home.e2e.js
@@ -1,14 +1,36 @@
 import puppeteer from 'puppeteer';
 
 describe('Homepage', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+  });
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/user/login', { waitUntil: 'networkidle2' });
+    // await page.evaluate(() => window.localStorage.setItem('antd-pro-authority', 'guest'));
+
+    // login
+    await page.waitForSelector('#email', {
+      timeout: 2000,
+    });
+    await page.type('#email', 'admin@example.org');
+    await page.type('#password', 'secret');
+    await page.click('button[type="submit"]');
+    await page.waitForSelector('#logo h1');
+  });
+
+  afterEach(() => page.close());
+
   it('it should have logo text', async () => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-    const page = await browser.newPage();
-    await page.goto('http://localhost:8000', { waitUntil: 'networkidle2' });
+    await page.goto('http://localhost:3000', { waitUntil: 'networkidle2' });
     await page.waitForSelector('#logo h1');
     const text = await page.evaluate(() => document.body.innerHTML);
     expect(text).toContain('<h1>Ant Design Pro</h1>');
-    await page.close();
-    browser.close();
   });
+
+  afterAll(() => browser.close());
 });

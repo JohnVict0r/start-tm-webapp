c2cd4f2b216778b8a21de4fa3f2f83d8a1bddd68
陈帅 <qixian.cs@outlook.com>
2018-10-08 23:07:45 -0300

    fix #2279, ga add judge
    (cherry picked from commit 21c252c8da9fb884908a52edca782bc772824e6e)

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 // https://umijs.org/config/
 import os from 'os';
 import pageRoutes from './router.config';
-import webpackplugin from './plugin.config';
+import webpackPlugin from './plugin.config';
 import defaultSettings from '../src/defaultSettings';
 
 export default {
@@ -40,6 +40,7 @@ export default {
       'umi-plugin-ga',
       {
         code: 'UA-72788897-6',
+        judge: () => window.location.hostname === 'preview.pro.ant.design',
       },
     ],
   ],
@@ -109,7 +110,7 @@ export default {
     ],
   },
 
-  chainWebpack: webpackplugin,
+  chainWebpack: webpackPlugin,
   cssnano: {
     mergeRules: false,
   },

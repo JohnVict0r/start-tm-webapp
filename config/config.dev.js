4fd10380c4f4e4c191d8477a3050adc2f82df829
Rayron Victor <rayronvictor@gmail.com>
2018-10-29 16:09:37 -0300

    CI: stage review

---
+++ config/config.dev.js
@@ -0,0 +1,4 @@
+export default {
+  base: '/test/',
+  publicPath: 'http://devel7.sedis.ufrn.br/test/',
+};
---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+$releases_dir = '/mnt/devel7/apps/produtiivo-web-client-pro/releases';
+$app_dir = 'mnt/devel7/apps/produtiivo-web-client-pro';
+$release = date('YmdHis');
+$new_release_dir = $releases_dir .'/'. $release;
+
+echo 'Cloning repository'
+[ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
+git clone --depth 1 {{ $CI_REPOSITORY_URL }} {{ $new_release_dir }}
+
+echo 'Linking current release'
+ln -nfs {{ $new_release_dir }} {{ $app_dir }}/current

1543f49b565eda0b8b0806214c61af51511a2ec2
Rayron Victor <rayronvictor@gmail.com>
2018-10-30 08:28:39 -0300

    CI: stage review

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 #$releases_dir = '/mnt/devel7/apps/produtiivo-web-client-pro/releases';
 #$app_dir = 'mnt/devel7/apps/produtiivo-web-client-pro';
 
-$releases_dir = $REMOTE_APP_DIR.'/releases';
-$release = date('YmdHis');
-$new_release_dir = $releases_dir .'/'. $release;
+releases_dir="$REMOTE_APP_DIR/releases";
+release=$CI_COMMIT_REF_SLUG;
+new_release_dir=$releases_dir/$release;
 
 echo 'Cloning repository'
-[ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
-git clone --depth 1 {{ $CI_REPOSITORY_URL }} {{ $new_release_dir }}
+[ -d $releases_dir ] || mkdir $releases_dir
+git clone --depth 1 $CI_REPOSITORY_URL $new_release_dir
 
 echo 'Linking current release'
-ln -nfs {{ $new_release_dir }} {{ $REMOTE_APP_DIR }}/current
+ln -nfs $new_release_dir "$REMOTE_APP_DIR/current"

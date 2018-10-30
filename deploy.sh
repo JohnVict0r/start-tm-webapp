1c0c1a9f30357bc50cf5f8c7947e82d597306328
Rayron Victor <rayronvictor@gmail.com>
2018-10-30 09:00:07 -0300

    CI: stage review

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,10 @@
 #$releases_dir = '/mnt/devel7/apps/produtiivo-web-client-pro/releases';
 #$app_dir = 'mnt/devel7/apps/produtiivo-web-client-pro';
 
+echo "antes"
+echo $REMOTE_APP_DIR
+echo "depois"
+
 releases_dir="$REMOTE_APP_DIR/releases";
 release=$CI_COMMIT_REF_SLUG;
 new_release_dir=$releases_dir/$release;

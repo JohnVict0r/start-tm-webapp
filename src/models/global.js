8fa9d04cdc494566731d1ed2d72f02ecd9a40623
Anyuxuan <anyuxuan0701@163.com>
2018-11-16 04:47:47 -0300

    feat: add count prop for NoticeIconTab component (#2862)
    * feat: add count prop for NoticeIconTab component.(ant-design-pro#2216)
    prop count is not required.
    
    * feat: unread notice message demo
    
    (cherry picked from commit 3fffc83d709d0dc3ad018f9a04cc5a81a904cda7)

--- src/models/global.js
+++ src/models/global.js
@@ -38,15 +38,21 @@ export default {
       });
     },
 
-    *fetchNotices(_, { call, put }) {
+    *fetchNotices(_, { call, put, select }) {
       const data = yield call(queryNotices);
       yield put({
         type: 'saveNotices',
         payload: data,
       });
+      const unreadCount = yield select(
+        state => state.global.notices.filter(item => !item.read).length
+      );
       yield put({
         type: 'user/changeNotifyCount',
-        payload: data.length,
+        payload: {
+          totalCount: data.length,
+          unreadCount,
+        },
       });
     },
     *clearNotices({ payload }, { put, select }) {
@@ -55,9 +61,37 @@ export default {
         payload,
       });
       const count = yield select(state => state.global.notices.length);
+      const unreadCount = yield select(
+        state => state.global.notices.filter(item => !item.read).length
+      );
       yield put({
         type: 'user/changeNotifyCount',
-        payload: count,
+        payload: {
+          totalCount: count,
+          unreadCount,
+        },
+      });
+    },
+    *changeNoticeReadState({ payload }, { put, select }) {
+      const notices = yield select(state =>
+        state.global.notices.map(item => {
+          const notice = { ...item };
+          if (notice.id === payload) {
+            notice.read = true;
+          }
+          return notice;
+        })
+      );
+      yield put({
+        type: 'saveNotices',
+        payload: notices,
+      });
+      yield put({
+        type: 'user/changeNotifyCount',
+        payload: {
+          totalCount: notices.length,
+          unreadCount: notices.filter(item => !item.read).length,
+        },
       });
     },
   },

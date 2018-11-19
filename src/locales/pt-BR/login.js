72f7019b3063ad0188e1cfe9dbde3548651ef026
Rayron Victor <rayronvictor@gmail.com>
2018-11-19 10:10:55 -0300

    organização da pasta locale

---
+++ src/locales/pt-BR/login.js
@@ -0,0 +1,13 @@
+export default {
+  'app.login.message-invalid-credentials':
+    'Nome de usuário ou senha inválidosd（admin/ant.design）',
+  'app.login.message-invalid-verification-code': 'Código de verificação inválido',
+  'app.login.tab-login-credentials': 'Credenciais',
+  'app.login.tab-login-mobile': 'Telefone',
+  'app.login.remember-me': 'Lembre-me',
+  'app.login.forgot-password': 'Esqueceu sua senha?',
+  'app.login.sign-in-with': 'Login com',
+  'app.login.login': 'Login',
+  'app.login.signup': 'Cadastre-se',
+  'app.login.backlogin': 'Voltar ao Login',
+};
---
+++ src/locales/pt-BR/validation.js
@@ -0,0 +1,17 @@
+export default {
+  'validation.name.required': 'Por favor insira um nome!',
+  'validation.email.required': 'Por favor insira seu email!',
+  'validation.email.wrong-format': 'O email está errado!',
+  'validation.password.required': 'Por favor insira sua senha!',
+  'validation.password.twice': 'As senhas não estão iguais!',
+  'validation.password.strength.msg':
+    'Por favor insira pelo menos 6 caracteres e não use senhas fáceis de adivinhar.',
+  'validation.password.strength.strong': 'Força: forte',
+  'validation.password.strength.medium': 'Força: média',
+  'validation.password.strength.short': 'Força: curta',
+  'validation.confirm-password.required': 'Por favor confirme sua senha!',
+  'validation.phone-number.required': 'Por favor insira seu telefone!',
+  'validation.phone-number.wrong-format': 'Formato de telefone errado!',
+  'validation.verification-code.required': 'Por favor insira seu código de verificação!',
+  'validation.email.mailnotfound': 'Email não existente',
+};

3c529fb3e2a6911f8be987f1b7faf36c93a7f093
Rayron Victor <rayronvictor@gmail.com>
2018-11-28 16:58:30 -0300

    style(Layout): altura do TopNavBar configurável pelas configurações

--- src/components/GlobalHeader/index.js
+++ src/components/GlobalHeader/index.js
@@ -31,11 +31,9 @@ export default class GlobalHeader extends PureComponent {
             <img src={logo} alt="logo" width="32" />
           </Link>
         )}
-        <Icon
-          className={styles.trigger}
-          type={collapsed ? 'menu-unfold' : 'menu-fold'}
-          onClick={this.toggle}
-        />
+        <span className={styles.trigger}>
+          <Icon type={collapsed ? 'menu-unfold' : 'menu-fold'} onClick={this.toggle} />
+        </span>
         <RightContent {...this.props} />
       </div>
     );
--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -2,6 +2,7 @@ module.exports = {
   navTheme: 'light', // theme for nav menu
   primaryColor: '#1890FF', // primary color of ant design
   layout: 'topmenu', // nav menu position: sidemenu or topmenu
+  layoutHeaderHeight: '48px', // top nav menu height
   contentWidth: 'Fluid', // layout of content: Fluid or Fixed, only works when layout is topmenu
   fixedHeader: true, // sticky header
   autoHideHeader: false, // auto hide header

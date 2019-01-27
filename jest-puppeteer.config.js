fde5616f1bb7c618d9581ffecb214ad0095d49df
xiaoyuhen <xiaoyuhen1994@gmail.com>
2019-01-27 04:44:46 -0300

    chore: upgrade jest-puppeteer and remove some puppeteer config
    (cherry picked from commit fe94317e41a1fefe0a1ff8e60f587e4d9bfb65ca)

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,13 +1,6 @@
 // ps https://github.com/GoogleChrome/puppeteer/issues/3120
 module.exports = {
   launch: {
-    args: [
-      '--disable-gpu',
-      '--disable-dev-shm-usage',
-      '--disable-setuid-sandbox',
-      '--no-first-run',
-      '--no-sandbox',
-      '--no-zygote',
-    ],
+    args: ['--disable-gpu', '--disable-dev-shm-usage', '--no-first-run', '--no-zygote'],
   },
 };

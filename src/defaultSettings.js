535458a577b484a78b26eabb9e0b9778f91646d3
Rayron Victor <rayronvictor@gmail.com>
2019-03-11 16:24:48 -0300

    defaultSettings: title

--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -9,7 +9,7 @@ module.exports = {
   menu: {
     disableLocal: false,
   },
-  title: 'Produtiivo',
+  title: 'The manager',
   pwa: true,
   // your iconfont Symbol Scrip Url
   // eg：//at.alicdn.com/t/font_1039637_btcrd5co4w.js

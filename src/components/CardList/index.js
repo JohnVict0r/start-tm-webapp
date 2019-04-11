d1ce9af5495dd9f203a10ef8d28ecdb83c263f15
Rayron Victor <rayronvictor@gmail.com>
2019-04-11 09:55:45 -0300

    CardList: status no header

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
-import { Button, Icon, List, Tag } from 'antd';
+import { Badge, Button, Icon, List, Tooltip } from 'antd';
 import { Draggable, Droppable } from 'react-beautiful-dnd';
 import NaturalDragAnimation from 'natural-drag-animation-rbdnd';
 import { Scrollbars } from 'react-custom-scrollbars';
@@ -11,40 +11,55 @@ import CardItem from './CardItem';
 import styles from './index.less';
 
 
-const CardList = ({ cardList, isDisabled, items, onClickNewCard }) => (
-  <ColumnList isDisabled={isDisabled}>
-    <ColumnList.Header
-      title={cardList.name}
-      action={[
-        <Tag key='1' color={cardList.status.color}>{cardList.status.displayName}</Tag>,
-        <Icon key='2' type="ellipsis" />
-      ]}
-    />
-    <Scrollbars autoHeight autoHeightMin={400} autoHeightMax={800} className={styles.scroll}>
-      <DroppableZone
-        droppableId={cardList.id.toString()}
-        type="CARD"
-        isDropDisabled={isDisabled}
+const CardList = ({ cardList, isDisabled, items, onClickNewCard }) => {
+
+  const title = (
+    <>
+      <Tooltip
+        title={`Status: ${cardList.status.displayName}`}
+        arrowPointAtCenter
+        mouseEnterDelay={1}
       >
-        <InnerCardList cards={items} />
-      </DroppableZone>
-    </Scrollbars>
-    { cardList.canCreateCard && (
-      <ColumnList.Footer
-        action={(
-          <Button
-            onClick={onClickNewCard}
-            icon="plus"
-            size="small"
-            block
-          >
-            Adicionar Tarefa
-          </Button>
-        )}
+        <Badge color={cardList.status.color} />
+      </Tooltip>
+      {cardList.name}
+    </>
+  );
+
+  return (
+    <ColumnList isDisabled={isDisabled}>
+      <ColumnList.Header
+        title={title}
+        action={[
+          <Icon key='1' type="ellipsis" />
+        ]}
       />
-    )}
-  </ColumnList>
-);
+      <Scrollbars autoHeight autoHeightMin={400} autoHeightMax={800} className={styles.scroll}>
+        <DroppableZone
+          droppableId={cardList.id.toString()}
+          type="CARD"
+          isDropDisabled={isDisabled}
+        >
+          <InnerCardList cards={items} />
+        </DroppableZone>
+      </Scrollbars>
+      { cardList.canCreateCard && (
+        <ColumnList.Footer
+          action={(
+            <Button
+              onClick={onClickNewCard}
+              icon="plus"
+              size="small"
+              block
+            >
+              Adicionar Tarefa
+            </Button>
+          )}
+        />
+      )}
+    </ColumnList>
+  );
+}
 
 const DroppableZone = ({ children, droppableId, type, isDropDisabled }) => (
   <Droppable droppableId={droppableId} type={type} isDropDisabled={isDropDisabled}>

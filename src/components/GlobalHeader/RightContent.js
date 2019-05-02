8ef6a1c93a7781aaaddf4460fac0beb2b85b8f16
Rayron Victor <rayronvictor@gmail.com>
2019-05-02 11:00:25 -0300

    Remove temporariamente: Notifição e barra de busca global

--- src/components/GlobalHeader/RightContent.js
+++ src/components/GlobalHeader/RightContent.js
@@ -119,7 +119,7 @@ export default class GlobalHeaderRight extends PureComponent {
     }
     return (
       <div className={className}>
-        <HeaderSearch
+        {/* <HeaderSearch
           className={`${styles.action} ${styles.search}`}
           placeholder={formatMessage({ id: 'component.globalHeader.search' })}
           dataSource={[
@@ -133,14 +133,14 @@ export default class GlobalHeaderRight extends PureComponent {
           onPressEnter={value => {
             console.log('enter', value); // eslint-disable-line
           }}
-        />
+        /> */}
         <FavoriteList
           className={styles.action}
           loading={fetchingFavorites}
           favorites={this.getFavoriteData()}
           onFavoriteVisibleChange={onFavoriteVisibleChange}
         />
-        <NoticeIcon
+        {/* <NoticeIcon
           className={styles.action}
           count={currentUser.unreadCount}
           onItemClick={(item, tabProps) => {
@@ -185,7 +185,7 @@ export default class GlobalHeaderRight extends PureComponent {
             emptyImage="https://gw.alipayobjects.com/zos/rmsportal/HsIsxMZiWKrNUavQUXqx.svg"
             showViewMore
           />
-        </NoticeIcon>
+        </NoticeIcon> */}
         {currentUser.name ? (
           <HeaderDropdown overlay={accountMenu}>
             <span className={`${styles.action} ${styles.account}`}>

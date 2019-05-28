4dfd0d6b05416507f473bf94e14144ab056143a4
Rayron Victor <rayronvictor@gmail.com>
2019-05-28 10:05:26 -0300

    Tasks: duração total das atividades de uma tarefa

--- src/pages/Cards/Tasks.js
+++ src/pages/Cards/Tasks.js
@@ -1,11 +1,15 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
-import { Button, Form, Input, TimePicker, List } from 'antd';
+import { Button, Collapse, Form, Input, TimePicker, Typography, List } from 'antd';
 import moment from 'moment';
 import { loggedInUserSelector } from '@/selectors/global';
 import EditableSection from './EditableSection';
 
-import styles from './Milestone.less';
+import styles from './Tasks.less';
+
+const formatDuration = duration => {
+  return `${Math.floor(duration / 60)}h${duration % 60}m`;
+};
 
 @connect(state => ({
   submitting: state.loading.effects['cards/updateMilestone'],
@@ -17,9 +21,9 @@ class Tasks extends PureComponent {
     e.preventDefault();
     const { form } = this.props;
     form.validateFields({ force: true }, (err, values) => {
-      const duration = moment.duration(values.duration.format('HH:mm')).asMinutes();
       if (!err) {
         const { dispatch, card } = this.props;
+        const duration = moment.duration(values.duration.format('HH:mm')).asMinutes();
         dispatch({
           type: 'cards/saveTask',
           payload: {
@@ -34,8 +38,6 @@ class Tasks extends PureComponent {
             form.resetFields();
           }
         });
-      } else {
-        form.resetFields();
       }
     });
   };
@@ -63,17 +65,22 @@ class Tasks extends PureComponent {
       },
     };
 
-    const format = 'HH:mm';
-
     return (
       <Form className={styles.form} onSubmit={this.handleSubmit}>
+        <Typography.Paragraph type="secondary">
+          Registre uma atividade informando o tempo gasto para executá-la
+        </Typography.Paragraph>
         <Form.Item {...formItemLayout}>
-          {getFieldDecorator('description', {})(<Input maxLength={255} placeholder="Descrição" />)}
+          {getFieldDecorator('description', {
+            rules: [{ required: true, message: 'Informe uma descrição!' }],
+          })(<Input maxLength={255} placeholder="Descrição" />)}
         </Form.Item>
         <Form.Item {...formItemLayout}>
-          {getFieldDecorator('duration', {})(
+          {getFieldDecorator('duration', {
+            rules: [{ required: true, message: 'Informe a duração da atividade!' }],
+          })(
             <TimePicker
-              format={format}
+              format="HH:mm"
               placeholder="Duração"
               minuteStep={5}
               style={{ width: '100%' }}
@@ -89,38 +96,53 @@ class Tasks extends PureComponent {
     );
   };
 
-  render() {
-    const {
-      card: { tasks },
-      currentUser,
-    } = this.props;
+  renderTaskItem = item => {
+    const { currentUser } = this.props;
     return (
-      <EditableSection title="Atividades" editingComponent={this.renderEditing()}>
+      <List.Item>
+        <div>
+          <Typography.Paragraph strong>{item.description}</Typography.Paragraph>
+          <Typography.Text type="secondary">
+            {'Duração de '}
+            <b>{formatDuration(item.duration)}</b>
+          </Typography.Text>
+          <div>
+            <Typography.Text type="secondary">
+              Por <b>{item.creator.name}</b>
+            </Typography.Text>
+          </div>
+          {currentUser.id === item.creator.id && (
+            <div style={{ textAlign: 'right' }}>
+              <a onClick={() => this.handleDelete(item.id)}>Remover</a>
+            </div>
+          )}
+        </div>
+      </List.Item>
+    );
+  };
+
+  render() {
+    const { card } = this.props;
+    const { tasks, tasksTotalDuration } = card;
+
+    const totalDuration = (
+      <span>
+        Duração total: <b>{formatDuration(tasksTotalDuration)}</b>
+      </span>
+    );
+
+    return (
+      <EditableSection
+        title="Atividades"
+        actionText="Adicionar"
+        editingComponent={this.renderEditing()}
+      >
         {tasks && tasks.length > 0 ? (
-          <List
-            itemLayout="horizontal"
-            dataSource={tasks}
-            renderItem={item => (
-              <List.Item
-                actions={[
-                  currentUser.id === item.creator.id && (
-                    <a onClick={() => this.handleDelete(item.id)}>Remover</a>
-                  ),
-                ]}
-              >
-                <List.Item.Meta
-                  // avatar={<Avatar src={item.creator.avatar} />}
-                  title={item.creator.name}
-                  description={item.description}
-                />
-                {`Duração: ${moment
-                  .utc()
-                  .startOf('day')
-                  .add({ minutes: item.duration })
-                  .format('H:mm')}`}
-              </List.Item>
-            )}
-          />
+          <Collapse className={styles.collapse} bordered={false} expandIconPosition="right">
+            <Collapse.Panel header={totalDuration} key="1">
+              <List itemLayout="horizontal" dataSource={tasks} renderItem={this.renderTaskItem} />
+            </Collapse.Panel>
+          </Collapse>
         ) : (
           'Não há atividades'
         )}

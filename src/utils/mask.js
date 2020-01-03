c115f4baea8f1e1e3930989c2a09c61f3f24e7bf
John Victor <johnvictorio3@gmail.com>
2020-01-03 07:19:59 -0300

    upperCaseMask added

--- src/utils/mask.js
+++ src/utils/mask.js
@@ -17,3 +17,7 @@ export const cepMask = value => {
 export const numberMask = value => {
   return value.replace(/\D/g, ''); // substitui qualquer caracter que nao seja numero por nada
 };
+
+export const upperCaseMask = value => {
+  return value.toUpperCase(); // transforma em letra maiúscula
+};
